73502eba54cb2fa14efebecfdf8aab063c55dd69
Angela Byrne <ange@convert2freedom.com>
2025-01-09 09:14:07 +1300

    disable eslint

--- next.config.js
+++ next.config.js
@@ -1,9 +1,16 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    reactStrictMode: true,
-    experimental: {
-      appDir: true,
+    eslint: {
+        // Warning: This allows production builds to successfully complete even if
+        // your project has ESLint errors.
+        ignoreDuringBuilds: true,
     },
-  }
-  
-  module.exports = nextConfig
+    typescript: {
+        // !! WARN !!
+        // Dangerously allow production builds to successfully complete even if
+        // your project has type errors.
+        ignoreBuildErrors: true,
+    },
+}
+
+module.exports = nextConfig
